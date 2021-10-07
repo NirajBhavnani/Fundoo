6f2d04a4d1404ec6bdbb640aaf7c0a943a70cb83
Niraj Bhavnani <niraj.bhavnani@morningstar.com>
2021-10-07 07:30:15 +0530

    [Niraj]: Vuex practice (Adding and deleting todos)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,16 @@
 <template>
   <div class="home">
-    <Todos />
+    <h1>Home</h1>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Todos from '@/components/Todos/Todos.vue'
 
 export default {
   name: 'Home',
   components: {
-    Todos
+    
   }
 }
 </script>
---
+++ src/views/VuexPractice.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="vuex-practice">
+    <Todos />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Todos from '@/components/Todos/Todos.vue'
+
+export default {
+  name: 'VueX',
+  components: {
+    Todos
+  }
+}
+</script>
