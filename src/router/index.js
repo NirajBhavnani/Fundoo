8d4b4d38dae8b9b747782c7918a4eb9a0bd2b7e0
Niraj Bhavnani <niraj.bhavnani@morningstar.com>
2021-10-07 22:08:12 +0530

    [Niraj]: Login Page implemented

--- src/router/index.js
+++ src/router/index.js
@@ -20,10 +20,7 @@ const routes = [
   {
     path: '/vuex',
     name: 'VueX',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/VuexPractice.vue')
+    component: () => import('../views/VuexPractice.vue')
   }
 ]
 
